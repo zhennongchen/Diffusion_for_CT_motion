e8126a5f1b7f8a90abe75d252f433c7b6f2c5b0a
Dufan Wu <dwu6@mgh.harvard.edu>
2025-01-12 00:43:23 +0000

    recreate docker

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -3,23 +3,21 @@
 # change the following variables to set up code, input and output dirs
 #HOST_WORK_DIR="/home/local/PARTNERS/dw640/docker_example"  # change this to your code directory
 #HOST_DATA_DIR="/home/local/PARTNERS/dw640/mnt/women_health_internal/DBT"  # change this to your data directory
-HOST_WORK_DIR="/home/local/PARTNERS/zc13/Documents"
-HOST_DATA_DIR="/home/local/PARTNERS/zc13/mnt"
-
+HOST_WORK_DIR="/home/local/PARTNERS/dw640/Diffusion_for_CT_motion"
+HOST_DATA_DIR="/home/local/PARTNERS/dw640/mnt/CAMCA/home/dufan.wu/research_output/ct_motion_diffusion"
 
 # get host user id to populate to the container
 HOST_USER_ID="$(id -u)"
 HOST_GROUP_ID="$(id -g)"
 HOST_USER_NAME=${USER}
-CONTAINER_WORKDIR="/workspace/Documents"   #/workspace/CTProjector" 
-CONTAINER_DATADIR="/mnt" #should be /mnt/data when using NAS drive   
+CONTAINER_WORKDIR="/workspace/Diffusion_for_CT_motion" 
+CONTAINER_DATADIR="/data" #should be /data when using NAS drive   
 
-sudo docker run -it --gpus=all --name=docker_ex --network="bridge" --shm-size=256m --ipc=host \
+sudo docker run -it --rm --gpus=all --name=ct_motion_diffusion --network="bridge" --shm-size=256m --ipc=host \
 -v ${HOST_WORK_DIR}:${CONTAINER_WORKDIR} \
 -v ${HOST_DATA_DIR}:${CONTAINER_DATADIR} \
--p 8120:8120 \
 -e CONTAINER_UID=${HOST_USER_ID} \
 -e CONTAINER_GID=${HOST_GROUP_ID} \
 -e CONTAINER_UNAME=${HOST_USER_NAME} \
 -e CONTAINER_WORKDIR=${CONTAINER_WORKDIR} \
-zc_sam:1.0
+ct_motion_diffusion/inference:1.0
